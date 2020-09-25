9fffa827b8377f3b36f86ad9525855ea3acf4d14
Alexander <bam85g@gmail.com>
2020-09-25 13:37:40 +0500

    Types are added

---
+++ pkg/types/types.go
@@ -0,0 +1,25 @@
+package types
+
+// Money представляет собой денежную сумму в минимальных единицах (центы, копейки, дирамы и т.д).
+type Money int64
+
+// Category представляет собой категорию, в которой был совершен платёж (авто, аптеки, рестараны и т.д).
+type Category string
+
+// Status представляет собой статус платежа.
+type Status string
+
+// Предопределенные статусы платежей.
+const (
+	StatusOk       Status = "OK"
+	StatusFail     Status = "FAIL"
+	StatusProgress Status = "INPROGRESS"
+)
+
+// Payment представляет информацию о платеже.
+type Payment struct {
+	ID       int
+	Amount   Money
+	Category Category
+	Status   Status
+}
